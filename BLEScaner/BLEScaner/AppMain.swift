f8baf95d55e941c03d2ee3958784ea6d5920dc3e
Artem Poluyanovich <a.polyuanovich@softteco.com>
2023-02-11 16:05:45 +0100

    init commit

---
+++ BLEScaner/BLEScaner/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  BLEScaner
+//
+//  Created by Artem Poluyanovich on 11.02.23.
+//
+
+import SwiftUI
+
+@main
+struct BLEScanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLEScaner/BLEScaner/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BLEScaner
+//
+//  Created by Artem Poluyanovich on 11.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
